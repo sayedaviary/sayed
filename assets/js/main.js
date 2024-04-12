8718192591bf44f8df1248da15469f0c78bb6005
sayedaviary <166824524+sayedaviary@users.noreply.github.com>
2024-04-12 19:50:54 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,23 +6,7 @@
 * License: https://bootstrapmade.com/license/
 */
 
-document.addEventListener('DOMContentLoaded', () => {
-  "use strict";
 
-  /**
-   * Preloader
-   */
-  const preloader = document.querySelector('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      setTimeout(() => {
-        preloader.classList.add('loaded');
-      }, 1000);
-      setTimeout(() => {
-        preloader.remove();
-      }, 2000);
-    });
-  }
 
   /**
    * Mobile nav toggle
