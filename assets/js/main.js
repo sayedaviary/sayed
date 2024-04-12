5f0491cb3dc6a7b90c860acaac64c7b7597237c3
sayedaviary <166824524+sayedaviary@users.noreply.github.com>
2024-04-12 20:15:34 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,55 @@
 /**
  * Mobile nav toggle
  */
-const mobileNavShow = document.querySelector('.mobile-nav-show');
-const mobileNavHide = document.querySelector('.mobile-nav-hide');
+const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      setTimeout(() => {
+        preloader.classList.add('loaded');
+      }, 1000);
+      setTimeout(() => {
+        preloader.remove();
+      } );
+    });
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  const mobileNavShow = document.querySelector('.mobile-nav-show');
+  const mobileNavHide = document.querySelector('.mobile-nav-hide');
+
+  document.querySelectorAll('.mobile-nav-toggle').forEach(el => {
+    el.addEventListener('click', function(event) {
+      event.preventDefault();
+      mobileNavToogle();
+    })
+  });
+
+  function mobileNavToogle() {
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    mobileNavShow.classList.toggle('d-none');
+    mobileNavHide.classList.toggle('d-none');
+  }
+
+  /**
+   * Hide mobile nav on same-page/hash links
+   */
+  document.querySelectorAll('#navbar a').forEach(navbarlink => {
+
+    if (!navbarlink.hash) return;
+
+    let section = document.querySelector(navbarlink.hash);
+    if (!section) return;
+
+    navbarlink.addEventListener('click', () => {
+      if (document.querySelector('.mobile-nav-active')) {
+        mobileNavToogle();
+      }
+    });
+
+  });
+
 
 document.querySelectorAll('.mobile-nav-toggle').forEach(el => {
   el.addEventListener('click', function(event) {
